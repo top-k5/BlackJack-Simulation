fe49788b8f485a6a127269f6fbf82ff0e28a9458
top-k5 <kimu.1515@akane.waseda.jp>
2025-02-21 01:03:25 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from game import Game
+
+def main():
+    game = Game()
+    while game.game_mode == 1:
+        game.reset_game()   # 各種リセット
+        game.bet(bet=100)   # 賭け金を設定
+        game.deal()         # カードを配る
+        game.player_turn()  # プレイヤーのターン
+        game.dealer_turn()  # ディーラーのターン
+        game.judge()        # 勝敗判定
+        game.pay_chip()     # 精算
+        game.check_chip()   # 残高確認
+        game.ask_next_game()# 続行確認
+        game.check_deck()   # デッキの残枚数確認
+
+    print("BlackJackを終了します")
+    print(f"{game.game_count}回ゲームをしました")
+
+if __name__ == '__main__':
+    main()
