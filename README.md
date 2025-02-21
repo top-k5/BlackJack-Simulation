b2f9def6adce40f1903b487c6891b0f67b6a1c8a
Keigo Kimura <kimu.1515@akane.waseda.jp>
2025-02-21 16:31:00 +0000

    残高推移のプロット

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Validate some strategies of BlackJack.
 
 # Todo
 ・毎ターンの残高を保持してプロット
-・Bet戦略の実装
 ・Strategyはクラスである必要ある？
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from src.game import Game
+from src.config import INITIAL_CHIP
+import matplotlib.pyplot as plt
 
 def main():
     global temp_debug
     temp_debug = False
     game = Game()
+    balance_history = [INITIAL_CHIP]
     while game.game_mode == 1:
         game.reset_game()   # 各種リセット
         game.bet()   # 賭け金を設定
@@ -15,9 +18,19 @@ def main():
         game.check_chip()   # 残高確認
         game.ask_next_game()# 続行確認
         game.check_deck()   # デッキの残枚数確認
+        balance_history.append(game.player.chip.balance)
 
     print("BlackJackを終了します")
     print(f"{game.game_count}回ゲームをしました")
+    print(balance_history)
+    
+    # 折れ線グラフで残高推移をプロット
+    plt.plot(balance_history)
+    plt.xlabel('Game Count')
+    plt.ylabel('Balance')
+    plt.title('Balance Transition')
+    plt.savefig('result/balance_history.jpg')
+    plt.close()
 
 if __name__ == '__main__':
     main()
